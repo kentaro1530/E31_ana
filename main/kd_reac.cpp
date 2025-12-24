8c08b9fdc7c92cbcd5f5f321930ffef434196108
井上謙太郎 <kentaro@rcnp.osaka-u.ac.jp>
2025-12-24 14:05:32 +0900

    方針転換

--- main/kd_reac.cpp
+++ main/kd_reac.cpp
@@ -1,7 +1,10 @@
 #include "W_to_mom.hh"
 
 #include "kd_reac/config.hh"
-#include "kd_reac/search_q0.cpp"
+#include "kd_reac/search_qdash.cpp"
+
+// template <class F>
+// double diff(F f, double x, double h = 1e-8){ return (f(x+h)-f(x-h))/(2.0*h); }
 
 int main(){
   std::cout<<"K- d reaction Calculation START"<<std::endl;
@@ -10,49 +13,74 @@ int main(){
   d_lmom.SetVectM(TVector3(0, 0, 0), dMass);
   TVector3 CM_to_lab = (beam_lmom+d_lmom).BoostVector();
 
-  double piS_mass = 1.43;
-  std::complex<double> n_mom = W_to_mom((beam_lmom+d_lmom).M(), piS_mass, nMass);
-  TLorentzVector n_lmom, piS_lmom;
-  n_lmom.SetVectM(TVector3(0, 0, n_mom.real()), nMass);
-  n_lmom.Boost(CM_to_lab);
-  piS_lmom.SetVectM(TVector3(0, 0, -n_mom.real()), piS_mass);
-  piS_lmom.Boost(CM_to_lab);
-  TVector3 lab_to_KN = -piS_lmom.BoostVector();
-  TVector3 P = piS_lmom.Vect();
-  //  piS_lmom.Boost(lab_to_KN);
+  //  for( double piS_mass = 1.35; piS_mass<1.6; piS_mass += 0.01 ){
+  double piS_mass = 1.50;
+    std::complex<double> n_mom = W_to_mom((beam_lmom+d_lmom).M(), piS_mass, nMass);
+    TLorentzVector n_lmom, piS_lmom;
+    n_lmom.SetVectM(TVector3(0, 0, n_mom.real()), nMass);
+    n_lmom.Boost(CM_to_lab);
+    piS_lmom.SetVectM(TVector3(0, 0, -n_mom.real()), piS_mass);
+    piS_lmom.Boost(CM_to_lab);
+    TVector3 lab_to_KN = -piS_lmom.BoostVector();
+    TVector3 P = piS_lmom.Vect();
+    double q0_2 = (pow(piS_mass, 2) - pow(onshell_mass-Kbar_mass, 2)) * (pow(piS_mass, 2) - pow(onshell_mass+Kbar_mass, 2))/(4*pow(piS_mass, 2));
+    const double E_N2_q0 = sqrt(q0_2+pow(onshell_mass, 2));
+    const double E_Kbar_q0 = sqrt(q0_2+pow(Kbar_mass, 2));
 
-  std::cout<<piS_mass<<std::endl;
-  //  std::cout<<n_lmom.X()<<", "<<n_lmom.Y()<<", "<<n_lmom.Z()<<std::endl;
-  std::cout<<"pi Sigma Vector = ( "<<piS_lmom.X()<<", "<<piS_lmom.Y()<<", "<<piS_lmom.Z()<<" )"<<std::endl;
-  std::cout<<"P = ( "<<P.X()<<", "<<P.Y()<<", "<<P.Z()<<" )"<<std::endl;
-  std::cout<<"lab_to_KN = ( "<<lab_to_KN.X()<<", "<<lab_to_KN.Y()<<", "<<lab_to_KN.Z()<<" )"<<std::endl;
-  double q_2 = (pow(piS_mass, 2) - pow(onshell_mass-Kbar_mass, 2)) * (pow(piS_mass, 2) - pow(onshell_mass+Kbar_mass, 2))/(4*pow(piS_mass, 2));
-  std::cout<<"q_2 = "<<q_2<<std::endl;
-  std::cout<<"W(q_2) = "<<sqrt(pow(onshell_mass, 2)+q_2)+sqrt(pow(Kbar_mass, 2)+q_2)<<std::endl;
-  
-  
-  double theta = 0.0, mom = 0.3;
-  //  for( double theta = 0.5*dtheta; theta<TMath::Pi(); theta += dtheta){
-  //    for( double mom = 0.0; mom<=max_mom; mom += dmom ){
-      TVector3 fermi_mom = TVector3(mom*sin(theta), 0.0, mom*cos(theta));
-      TLorentzVector onshell_lmom, offshell_lmom;
-      onshell_lmom.SetVectM(fermi_mom, onshell_mass);
-      offshell_lmom = d_lmom - onshell_lmom;
-      double q0_2 = search_q0(piS_mass);      
-      std::cout<<"q0_2 = "<<q0_2<<std::endl;
-      std::cout<<"W(q0_2) = "<<sqrt(pow(onshell_mass, 2)+q0_2)+sqrt(pow(Kbar_mass, 2)+q0_2)<<std::endl;
-      
-      //      TLorentzVector Kbar_lmom = beam_lmom + offshell_lmom - n_lmom;
-      //      Kbar_lmom.Boost(lab_to_KN);
-      //      onshell_lmom.Boost(lab_to_KN);
-
-      // std::cout<<"("<<Kbar_lmom.X()<<", "<<Kbar_lmom.Y()<<", "<<Kbar_lmom.Z()<<") :  m = "<<Kbar_lmom.M()<<std::endl;
-      // std::cout<<"("<<onshell_lmom.X()<<", "<<onshell_lmom.Y()<<", "<<onshell_lmom.Z()<<") :  m = "<<onshell_lmom.M()<<std::endl;
-      //    }
-      //  }
-
-  
-  
-  
+    std::cout<<"piS mass = "<<piS_mass<<"   q0_2 = "<<q0_2<<std::endl;
+    
+    for( double theta = 0.5*dtheta; theta<TMath::Pi(); theta += dtheta ){
+      std::cout<<">>> theta = "<<theta<<std::endl;
+      search_qdash0_from_q0(beam_lmom, n_lmom, piS_lmom, theta, q0_2);
+      //      for( double mom = 0; mom<1.0; mom+= 0.001 ){
+      //	std::cout<<"  > mom "<<mom<<"  q_dash : "<<search_qdash(beam_lmom, n_lmom, piS_lmom, mom, theta).Mag2()<<std::endl;
+      //      }
+    }
   std::cout<<"K- d reaction Calculation FINISH"<<std::endl;
 }
+    //    for( double mom = 0.5*dmom; mom<max_mom; mom += dmom ){
+
+
+
+	//	double theta = 0.5, mom = 0.3;
+    // 	TVector3 q_N2(mom*sin(theta), 0.0, mom*cos(theta));
+    // 	TVector3 q_Kbar = beam_lmom.Vect() - n_lmom.Vect() - q_N2;
+    // 	double E_lab_Kbar = sqrt(pow(Kbar_mass, 2)+q_Kbar.Mag2());
+    // 	double E_lab_N2 = sqrt(pow(onshell_mass, 2)+q_N2.Mag2());
+    // 	double E_CM_Kbar = sqrt(pow(Kbar_mass, 2));
+    // 	double E_CM_N2 = sqrt(pow(Kbar_mass, 2));
+    // 	double e_Kbar = 0.5*(E_lab_Kbar + E_CM_Kbar);
+    // 	double e_N2 = 0.5*(E_lab_N2 + E_CM_N2);
+    // 	TVector3 q_dash = -q_N2 + e_N2/(e_Kbar+e_N2)*P;
+
+    // 	int count=0;
+    // 	while( true ){
+    // 	  //	  std::cout<<"E_CM_Kbar(old) = "<<E_CM_Kbar<<std::endl;
+    // 	  //	  std::cout<<"E_CM_N2(new) = "<<E_CM_N2<<std::endl;
+    // 	  E_CM_Kbar = sqrt(pow(Kbar_mass, 2)+q_dash.Mag2());
+    // 	  E_CM_N2 = sqrt(pow(Kbar_mass, 2)+q_dash.Mag2());
+    // 	  e_Kbar = 0.5*(E_lab_Kbar + E_CM_Kbar);
+    // 	  e_N2 = 0.5*(E_lab_N2 + E_CM_N2);
+    // 	  TVector3 q_dash2 = -q_N2 + e_N2/(e_Kbar+e_N2)*P;
+    // 	  // std::cout<<"E_CM_Kbar(old) = "<<E_CM_Kbar<<std::endl;
+    // 	  // std::cout<<"E_CM_N2(new) = "<<E_CM_N2<<std::endl;
+    // 	  // std::cout<<"q'(old) = ( "<<q_dash.X()<<", "<<q_dash.Y()<<", "<<q_dash.Z()<<" )"<<std::endl;
+    // 	  // std::cout<<"q'(new) = ( "<<q_dash2.X()<<", "<<q_dash2.Y()<<", "<<q_dash2.Z()<<" )"<<std::endl;
+    // 	  if( (q_dash-q_dash2).Mag()<1e-15 ){
+    // 	    std::cout<<"Count = "<<count<<" loop calc finish  delta = "<<(q_dash-q_dash2).Mag()<<std::endl;
+    // 	    break;
+    // 	  }
+    // 	  if( count==MAX_ITERATION ){
+    // 	    throw std::runtime_error("q' Calculation not converged");
+    // 	  }
+    // 	  q_dash = q_dash2;
+    // 	  count++;
+    // 	}
+    // 	const double factor = 1/(E_N2_q0 + sqrt(pow(onshell_mass, 2)+q_dash.Mag2())) + 1/(E_Kbar_q0 + sqrt(pow(Kbar_mass, 2)+q_dash.Mag2()));
+    // 	const double W_q = sqrt(q_dash.Mag2()+pow(Kbar_mass, 2))+sqrt(q_dash.Mag2()+pow(onshell_mass, 2));
+    // 	const double prefactor = (sqrt(P.Mag2()+pow(piS_mass, 2))+sqrt(P.Mag2()+pow(W_q, 2)))/(piS_mass+W_q);
+//      }
+//    }
+//  }
+
+//}
--- main/kd_reac/config.hh
+++ main/kd_reac/config.hh
@@ -3,8 +3,12 @@
 
 const double N_theta = 10;
 const double dtheta = TMath::Pi()/N_theta;
+
 const double max_mom = 0.5;
 const double N_mom = 10;
 const double dmom = max_mom/N_mom;
+
 const double onshell_mass = nMass;
 const double Kbar_mass = kpMass;
+
+const int MAX_ITERATION = 50;
